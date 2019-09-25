5ced053366271192286e9375505b181227e8e345
arons121 <arons121@morris.umn.edu>
2019-09-25 15:27:13 -0500

    Finished heuristic search
    Fixed a function call with the wrong parameters, and changed concat to conj in order to preserve the vector in make-p-map
    
    Co-authored-by: Nathan Foss <fossx148@morris.umn.edu>

--- intro-to-ec/src/intro_to_ec/heuristic_search.clj
+++ intro-to-ec/src/intro_to_ec/heuristic_search.clj
@@ -13,31 +13,54 @@
   (+ (Math/abs (- (get curr 0)(get goal 0)))
      (Math/abs (- (get curr 1)(get goal 1)))))
 
-(defn pair [goal child map]
+(defn a-star-distance [start goal curr]
+  (+ (heuristic-distance start curr)
+     (heuristic-distance goal curr)))
+
+(defn pair-heuristic [goal child map]
   "Pairs nodes to distance from goal"
   (assoc map child (heuristic-distance goal child)))
 
-(defn make-p-map
-  [children goal map]
+(defn make-p-map-heuristic
+  [children goal map start]
   (if (not (empty? children))
     (do
-      (pair goal
-        (first children)
-        map)
-      (make-p-map (rest children) goal map)
-      (println map))
+      ; (pair goal
+      ;   (first children)
+      ;   map)
+      (make-p-map-heuristic (rest children) goal
+            (pair-heuristic goal
+              (first children)
+              map)
+                            start))
     map))
 
-; (defn make-p-map
-;   [children goal]
-;   "For child in children, make a priority map of their distance to the goal"
-;   (for [child children]
-;     (pair goal child helper-map)))
+(defn pair-a-star [goal child map start]
+  "Pairs nodes to distance from goal"
+  (assoc map child (a-star-distance start goal child)))
+
+(defn make-p-map-a-star
+  [children goal map start]
+  (if (not (empty? children))
+    (do
+      ; (pair goal
+      ;   (first children)
+      ;   map)
+      (make-p-map-a-star (rest children) goal
+                         (pair-a-star goal
+                               (first children)
+                               map
+                               start)
+                         start))
+    map))
 
 (def heuristic-search
   {:get-next-node first
-   :add-children #(concat %4 (for [x (make-p-map %1 %2 %3)]
-                               (first (first x))))})
+   :add-children #(conj %4 (first (first (make-p-map-heuristic %1 %2 %3 %5))))})
+
+(def a-star-search
+  {:get-next-node first
+   :add-children #(conj %4 (first (first (make-p-map-a-star %1 %2 %3 %5))))})
 
 (defn generate-path
   [came-from node]
@@ -55,7 +78,7 @@
          came-from {start-node :start-node}
          num-calls 0]
 
-    (println num-calls ": " frontier)
+    (println num-calls ": " frontier "a-star-distance: "(a-star-distance start-node goal-pos (get-next-node frontier)))
     ; (println came-from)
     (let [current-node (get-next-node frontier)]
       (cond
@@ -69,6 +92,7 @@
             kids
             goal-pos
             helper-map
-            (rest frontier))
+            (rest frontier)
+            start-node)
            (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
            (inc num-calls)))))))
